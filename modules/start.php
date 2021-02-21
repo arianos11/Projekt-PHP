da8f330e59a472b53855debff04f20dfa5c6c5d9
Arian Orwat <arian.orwat@gmail.com>
2021-02-21 22:12:33 +0100

    scss

--- modules/start.php
+++ modules/start.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel='stylesheet' href='../stylesheet/css/style.css'>
 </head>
 <body>
 
